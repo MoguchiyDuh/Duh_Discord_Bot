d81aebd6b83801fbf24b35a3dcc3bb52ed4a9ebb
Moguchiy_Duh <kirnekrasov12@gmail.com>
2024-12-17 03:42:23 +0300

    youtube name search

---
+++ discord_bot/main.py
@@ -0,0 +1,58 @@
+import os
+import discord
+import asyncio
+from discord.ext import commands
+from utils.config import DISCORD_TOKEN
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+async def load_cogs():
+    """Load all cogs from the cogs directory."""
+    cog_directory = os.path.join(os.getcwd(), "cogs")
+    cogs_loaded = []
+
+    for filename in os.listdir(cog_directory):
+        if (
+            filename.endswith(".py")
+            and filename != "__init__.py"
+            and "disabled" not in filename.lower()
+        ):
+            cog_name = f"cogs.{filename[:-3]}"
+            try:
+                await bot.load_extension(cog_name)
+                cogs_loaded.append(cog_name)
+            except Exception as e:
+                print(f"Failed to load cog {cog_name}: {e}")
+
+    return cogs_loaded
+
+
+@bot.event
+async def on_ready():
+    """Event handler when the bot is ready."""
+    print(f"Logged in as {bot.user}")
+    try:
+        # Syncing commands
+        sync_commands = await bot.tree.sync()
+        print(f"Synced {len(sync_commands)} commands.")
+    except Exception as e:
+        print(f"Failed to sync commands: {e}")
+
+
+async def run_bot():
+    """Load cogs and start the bot."""
+    try:
+        await load_cogs()  # Load cogs asynchronously
+        await bot.start(DISCORD_TOKEN)
+    except KeyboardInterrupt:
+        print("Bot was stopped by KeyboardInterrupt.")
+    except Exception as e:
+        print(f"Unexpected error occurred: {e}")
+    finally:
+        await bot.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(run_bot())  # Run the bot using asyncio
---
+++ discord_bot/utils/sources/yt_source.py
@@ -0,0 +1,85 @@
+import asyncio
+import discord
+import yt_dlp
+
+from . import Playlist, Track
+
+
+# --------------------------------------------------
+class TrackSelectButton(discord.ui.Button):
+    def __init__(self, label, track: Track):
+        super().__init__(label=label, style=discord.ButtonStyle.primary)
+        self.track = track
+
+    async def callback(self, interaction: discord.Interaction):
+        self.view.selected_track = self.track
+        await interaction.response.defer()
+        self.view.stop()
+
+
+class TrackSelectView(discord.ui.View):
+    def __init__(self, tracks: list[Track]):
+        super().__init__(timeout=60)
+        self.selected_track = None
+        for i, track in enumerate(tracks, start=1):
+            self.add_item(TrackSelectButton(label=str(i), track=track))
+
+
+# --------------------------------------------------
+
+
+class YTSource:
+    """YouTube audio source using yt-dlp (for both single song and playlists)."""
+
+    YTDL_OPTIONS = {
+        "no_warnings": True,  # Suppress warnings
+        "skip_download": True,  # Don't download anything
+        "extract_flat": True,  # Only extract metadata (e.g., title, duration, thumbnail)
+        "format": "bestaudio/best",  # For best audio info (doesn't affect title, duration, or thumbnail)
+        "noplaylist": False,  # Keep playlist support
+    }
+    ENTRIES_COUNT = 5  # Number of entries to fetch for search queries
+
+    async def fetch_track_by_url(self, url) -> Track:
+        """Fetches a single song by url"""
+
+        loop = asyncio.get_event_loop()
+        with yt_dlp.YoutubeDL(self.YTDL_OPTIONS) as ytdl:
+            track = await loop.run_in_executor(
+                None, lambda: ytdl.extract_info(url, download=False)
+            )
+            return self.__get_track(track)
+
+    def __get_track(self, entry) -> Track:
+        return Track(
+            title=entry.get("title", "Unknown Title"),
+            duration=entry.get("duration", 0),
+            webpage_url=entry.get("webpage_url", entry.get("url")),
+            audio_url=entry.get("url", ""),
+            thumbnail=entry.get("thumbnail", ""),
+        )
+
+    async def fetch_playlist(self, url) -> Playlist:
+        """Fetches a playlist"""
+        loop = asyncio.get_event_loop()
+        with yt_dlp.YoutubeDL(self.YTDL_OPTIONS) as ytdl:
+            entries = await loop.run_in_executor(
+                None, lambda: ytdl.extract_info(url, download=False)
+            )
+            return Playlist(
+                title=entries.get("title", "Unknown Title"),
+                webpage_url=entries.get("webpage_url"),
+                tracks=[self.__get_track(entry) for entry in entries["entries"]],
+            )
+
+    async def fetch_by_name(self, name) -> list[Track]:
+        """Fetches a single song by name"""
+        loop = asyncio.get_event_loop()
+        with yt_dlp.YoutubeDL(self.YTDL_OPTIONS) as ytdl:
+            entries = await loop.run_in_executor(
+                None,
+                lambda: ytdl.extract_info(
+                    f"ytsearch{self.ENTRIES_COUNT}:{name}", download=False
+                ),
+            )
+        return [self.__get_track(entry) for entry in entries["entries"]]
